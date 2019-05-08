e6ceabdcd812edaf8a77cebdf9d089b071eb0ef7
Matthew Holland <woodshoes@gmail.com>
2019-05-08 17:24:45 -0400

    added createnewusers component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ import SearchContainer from "./Search/SearchContainer";
 import ViewProfileContainer from "./ViewProfile/ViewProfileContainer";
 import NewProfileContainer from "./NewProfile/NewProfileContainer";
 import EditProfileContainer from "./EditProfile/EditProfileContainer";
-import NewUsers from "./CreateNewUsers/NewUsersContainer";
+import NewUsersContainer from "./CreateNewUsers/NewUsersContainer";
 
 function App() {
   return (
@@ -29,7 +29,7 @@ function App() {
           path="/profile/:graduateId"
           component={ViewProfileContainer}
         />
-        <Route exact path="/profile/createnewusers" component={NewUsers} />
+        <Route exact path="/newusers" component={NewUsersContainer} />
         <Redirect to="/" />
       </Switch>
       <Footer />
--- src/components/CreateNewUsers/NewUsers.jsx
+++ src/components/CreateNewUsers/NewUsers.jsx
@@ -1,49 +1,97 @@
 import React, { Component } from "react";
-import history from "../../history";
-import { Button, Modal, ModalDialog } from "react-bootstrap";
+//import history from "../../history";
+import { Button, Modal /*ModalDialog*/ } from "react-bootstrap";
 
 class NewUsers extends Component {
   state = {
-    showModal: false
+    showModal: false,
+    emailInput: ""
   };
 
-  handleClose() {
+  handleClose = () => {
     this.setState({ showModal: false });
-  }
+  };
 
-  handleAddEmails() {
+  handleAddEmails(e) {
     this.setState({ showModal: true });
   }
+
+  handleInput = e => {
+    this.setState({ emailInput: e.target.value });
+  };
+
+  emailValidation = () => {
+    let emailArray = [];
+    emailArray = this.state.emailInput.split(",");
+
+    let trimmedArray = [];
+    trimmedArray = emailArray.map(item => item.trim());
+
+    let numberOfAts = 0;
+    let numberOfCommas = 0;
+
+    if (this.state.emailInput.indexOf("@") !== -1) {
+      numberOfAts = this.state.emailInput.match(/@/g).length;
+    }
+
+    if (this.state.emailInput.indexOf(",") !== -1) {
+      numberOfCommas = this.state.emailInput.match(/,/g).length;
+    }
+
+    if (trimmedArray.length === 1 && numberOfAts === 1) {
+      alert("Thank you!");
+      this.setState({ showModal: false });
+      //submitEmails(trimmedArray);
+    } else if (numberOfAts - numberOfCommas !== 1) {
+      alert(
+        "Please check input. Make sure email addresses are separated by a comma."
+      );
+    } else {
+      alert("Thank you!");
+      this.setState({ showModal: false });
+      //submitEmails(trimmedArray);
+    }
+  };
+
+  // submitEmails = emailArray => {};
 
   render() {
     return (
       <div>
-        <Button variant="primary" onClick={this.handleAddEmails}>
-          Custom Width Modal
+        <Button variant="primary" onClick={e => this.handleAddEmails(e)}>
+          Add email addresses
         </Button>
 
         <Modal
-          show={this.state.show}
-          onHide={this.handleClose}
+          show={this.state.showModal}
+          onHide={e => this.handleClose(e)}
           dialogClassName="modal-90w"
           aria-labelledby="example-custom-modal-styling-title"
         >
-          <Modal.Header closeButton>
+          <Modal.Header>
             <Modal.Title id="example-custom-modal-styling-title">
-              Custom Modal Styling
+              Add email addresses, separated by a comma.
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <p>
-              Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae
-              unde commodi aspernatur enim, consectetur. Cumque deleniti
-              temporibus ipsam atque a dolores quisquam quisquam adipisci
-              possimus laboriosam. Quibusdam facilis doloribus debitis! Sit
-              quasi quod accusamus eos quod. Ab quos consequuntur eaque quo rem!
-              Mollitia reiciendis porro quo magni incidunt dolore amet atque
-              facilis ipsum deleniti rem!
-            </p>
+            <div class="form-group">
+              <textarea
+                class="form-control"
+                id="message-text"
+                onChange={e => this.handleInput(e)}
+                value={this.state.emailInput}
+              />
+            </div>
           </Modal.Body>
+
+          <Modal.Footer>
+            <Button variant="secondary" onClick={e => this.handleClose(e)}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={this.emailValidation}>
+              Add new users
+            </Button>
+          </Modal.Footer>
         </Modal>
       </div>
     );
